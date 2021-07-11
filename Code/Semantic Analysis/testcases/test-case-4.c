26f368de6355db171c13c790966ae20327cf2fb9
ManojMaheshPatil <54990161+ManojMaheshPatil@users.noreply.github.com>
2021-07-11 16:13:25 +0530

    Add files via upload

---
+++ Code/Semantic Analysis/testcases/test-case-4.c
@@ -0,0 +1,19 @@
+/*
+
+Test Case 4
+Invalid use of BREAK and CONTINUE
+
+*/
+
+
+int main()
+{
+    break; //invalid
+
+    int i;
+    for(i=1; i<10; i++){
+        int x;
+        continue;
+    }
+    /*continue; //invalid*/
+}
